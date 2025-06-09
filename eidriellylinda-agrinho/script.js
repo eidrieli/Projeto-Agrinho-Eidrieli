4eb58b505f13e59c74e5b4aa7985f7b678f2927c
eidrieli <145780395+eidrieli@users.noreply.github.com>
2025-06-09 11:41:17 -0300

    Add files via upload

---
+++ eidriellylinda-agrinho/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("participarBtn");
+
+  button.addEventListener("click", function () {
+    alert("Obrigado pelo interesse! Em breve enviaremos mais informações por e-mail.");
+  });
+});
